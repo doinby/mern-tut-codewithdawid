b2a2fc7bc6f2726b178288c2030dcd611135db82
Chinh Do <chinh@doinby.co>
2023-04-03 17:56:40 +0700

    Add function to post user data. Not working

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
+// Import model schema
+const User = require('./models/User');
 
 // Express App
 const app = express();
@@ -8,16 +12,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const username = process.env.MONGOOSE_USERNAME;
-const password = process.env.MONGOOSE_PASSWORD;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
 
 mongoose.connect(
 	`mongodb+srv://${username}:${password}@cluster0.nphp4yv.mongodb.net/?retryWrites=true&w=majority`
 );
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
 	const { email, password } = req.body;
-	res.json({ requestData: { email, password } });
+
+	// Save to MongoDB using model created in ./models/User.js
+	const userData = await User.create('email', 'password');
+	res.json('ok');
 });
 
 // Listen for request
